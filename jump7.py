02572eb4eef5e8d246e200de852ecf6bece1588e
Yangyanan <1508315526@qq.com>
2021-04-20 11:28:10 +0800

    first commit

---
+++ jump7.py
@@ -0,0 +1,20 @@
+a = 0
+whilea < 100:
+    a = a + 1
+    if a % 7 == 0 or a % 10 == 7 or a // 10 == 7 :
+        continue
+    else :
+        print ( a )
+# 逢7跳过  WHILE循环
+
+
+
+
+# 逢7跳过  FOR 循环
+
+
+# for a in range(1,101):
+#    if a % 7 == 0 or a % 10 == 7 or a // 10 == 7 :
+#        continue
+#    else:
+#        print ( a )  
